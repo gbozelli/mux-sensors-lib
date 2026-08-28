92bbd799832cb66577057545359fe7f1da0506ad
Gabriel Bozelli Dias <gabriel2004bozellidias@gmail.com>
2026-08-27 21:10:47 -0300

    Fix syntax errors in test.c

--- src/test/test.c
+++ src/test/test.c
@@ -1,9 +1,13 @@
+#include <stdio.h>
+
+int run_test(int test_id);
+
 int test_1(){
-  // ...
+  return 1;
 }
 
 int main(){
-  printf("Starting test...")
+  printf("Starting test...\n");
   int number_tests = 3;
   int count_passed_tests = 0;
   for (int i = 0; i < number_tests; i++) {
@@ -17,4 +21,8 @@ int main(){
   printf("Passed %d out of %d tests.\n", count_passed_tests, number_tests);
 
   return 0;
+}
+
+int run_test(int test_id) {
+    return 1; // Dummy implementation for now
 }
